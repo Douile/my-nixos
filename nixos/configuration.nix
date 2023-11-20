ca13b76081074819bad3248439f3679d383213bd
Douile <douile@douile.com>
2023-11-20 20:56:16 +0000

    Enable nix-store optimisation

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -196,6 +196,9 @@
     };
   };
 
+  # Enable storage optimisation
+  nix.optimise.automatic = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
